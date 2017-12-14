4101af75df1ba0352bad3637e37ed29a6a16721b
Grégoire Joncour <greg.joncour@gmail.com>
2017-12-14 13:49:26 +0100

    + beau

--- Assets/Block.cs
+++ Assets/Block.cs
@@ -14,10 +14,10 @@ public class Block
     public bool isSolid;
 
     Vector2[,] blockUVs = {
-        /*GRASS*/      {new Vector2(0.125f,0.375f), new Vector2(0.1875f,0.375f), new Vector2(0.125f, 0.4375f), new Vector2(0.1875f,0.4375f)},
-        /*GRASS SIDE*/ {new Vector2(0.1875f,0.9375f), new Vector2(0.25f,0.9375f), new Vector2(0.1875f, 1.0f), new Vector2(0.25f,1.0f)},
-        /*DIRT*/       {new Vector2(0.125f,0.9375f), new Vector2(0.1875f,0.9375f), new Vector2(0.125f, 1.0f), new Vector2(0.1875f,1.0f)},
-        /*STONE*/      {new Vector2(0, 0.875f), new Vector2(0.0625f,0.875f), new Vector2(0, 0.9375f), new Vector2(0.0625f,0.9375f)}
+        /*GRASS*/      {new Vector2(0.125f,0.375f),   new Vector2(0.1875f,0.375f),  new Vector2(0.125f, 0.4375f), new Vector2(0.1875f,0.4375f)},
+        /*GRASS SIDE*/ {new Vector2(0.1875f,0.9375f), new Vector2(0.25f,0.9375f),   new Vector2(0.1875f, 1.0f),   new Vector2(0.25f,1.0f)},
+        /*DIRT*/       {new Vector2(0.125f,0.9375f),  new Vector2(0.1875f,0.9375f), new Vector2(0.125f, 1.0f),    new Vector2(0.1875f,1.0f)},
+        /*STONE*/      {new Vector2(0, 0.875f),       new Vector2(0.0625f,0.875f),  new Vector2(0, 0.9375f),      new Vector2(0.0625f,0.9375f)}
     };
 
     public Block(BlockType b, Vector3 pos, GameObject p, Material c)
